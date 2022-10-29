247a9f16bbb776bdf169de122835602114853898
natkru <n9215841072@gmail.com>
2022-10-29 19:26:43 +0300

    правки хедера

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import {useState} from 'react';
-import headerLogo from '../../images/header_logo.svg';
+
 import './Header.css';
-import { Link } from "react-router-dom";
-import Navigation from "../Navigation/Navigation";
+import { Link, useLocation } from 'react-router-dom';
+import headerLogo from '../../images/header_logo.svg';
+import NavAuth from '../NavAuth/NavAuth';
+import Navigation from '../Navigation/Navigation';
 
 const Header = (props) => {
-
-    // const [showItems, setShowItems] = useState(false);
-    // const handleToggleMenu = () => setShowItems(!showItems);
-
     return (
-
         <header className="header">
             <div className={`header__body-auth-on ${props.authOn ? 'header__body-auth-on_active' : ''}`}>
-                <Link to="/" className="navigation__link">
-                    <img className="header__logo" src={headerLogo} alt="логотип"/>
+                <Link to="/landing" className="header__link">
+                    <img className="header__logo header__logo_auth-off" src={headerLogo} alt="логотип"/>
                 </Link>
+                
                 <div className="header__container">
                     <Navigation showItems={props.showItems} onClick={props.handleToggleMenu}/>
                 </div>
@@ -25,22 +21,9 @@ const Header = (props) => {
                     <span className={`${props.showItems ? 'header__burger_active' : ''}`}></span>
                 </button>
             </div>
-            <div className={`header__body-auth-off ${props.authOn ? '' : 'header__body-auth-off_active'}`}>
-                <img className="header__logo header__logo_auth-off" src={headerLogo} alt="логотип"/>
-                <ul className="header__container header__container_auth-off">
-                    <li className="header__item">
-                        <Link to="/signup" className="header__link header__link_auth-off">Регистрация</Link>
-                    </li>
-                    <li className="header__item">
-                        <button className="header__login-button" type="button" onClick={props.handleToggleMenu}>
-                            <Link to="/signin" className="header__profile-link header__profile-link_black">Войти</Link>
-                        </button>
-                    </li>
-                </ul>
-            </div>
+            <NavAuth authOn={props.authOn} onClick={props.handleToggleMenu}/>
         </header>
     );
-}
-
+};
 
 export default Header;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,12 @@
-import React from "react";
 import './Navigation.css';
-import {Link} from "react-router-dom";
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
-function Navigation(props) {
+const Navigation = (props) => {
+    const [showItems, setShowItems] = useState(false);
+
+    const handleToggleMenu = () => setShowItems(!showItems);
+
     return (
         <nav className={`navigation__menu ${props.showItems ? "navigation__menu_active" : ""}`}>
             <ul className="navigation__list">
@@ -20,7 +24,23 @@ function Navigation(props) {
                 <Link to="/profile" className="navigation__profile-link">Аккаунт</Link>
             </button>
         </nav>
+        // <nav className={`navigation__menu ${props.showItems ? "navigation__menu_active" : ""}`}>
+        //     <ul className="navigation__list">
+        //         <li className="">
+        //             <Link to="/" className="navigation__link">Главная</Link>
+        //         </li>
+        //         <li className="">
+        //             <Link to="/movies" className="navigation__link">Фильмы</Link>
+        //         </li>
+        //         <li className="">
+        //             <Link to="/saved-movies" className="navigation__link">Сохранённые фильмы</Link>
+        //         </li>
+        //     </ul>
+        //     <button className="navigation__profile-button" type="button" onClick={props.onClick}>
+        //         <Link to="/" className="navigation__profile-link">Аккаунт</Link>
+        //     </button>
+        // </nav>
     );
-}
+};
 
 export default Navigation;
