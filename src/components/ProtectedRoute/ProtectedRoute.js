fa1177ad6b1dc56b2af13a204c24ea0f11ca6245
natkru <n9215841072@gmail.com>
2022-11-10 23:47:56 +0300

    редикект на /

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,7 +4,7 @@ import Preloader from '../Preloader/Preloader';
 const ProtectedRoute = ({ component: Component, ...props }) => {
     return (
         <Route>
-            {props.loggedIn  ? <Component {...props} /> : <Redirect to="/signin"/> }
+            {props.loggedIn  ? <Component {...props} /> : <Redirect to="/"/> }
         </Route>
     );
 };
